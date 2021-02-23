9a0dfe8e6514f37a48d854cab048c55a567a7e32
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-02-22 19:14:40 -0500

    Update simpson.m

--- simpson.m
+++ simpson.m
@@ -1,6 +1,6 @@
 function I = simpson(f,a,b)
   
-  #Simpson -1/3 rule (single version).
+  # Simpson -1/3 rule (single version).
   
   format long
   h = (b-a)/2;
@@ -11,5 +11,17 @@ function I = simpson(f,a,b)
   format short
   I=h/3*(f(a)+f(b)+4*f((a+b)/2))
   fprintf('\n')
+  
+  # Example:
+  # I = simpson(@(x)log(x),1,2)
+  
+  # WE GET:
+  # LONG FORMAT
+  # I = 3.858346021654338e-01
+  # SHORT FORMAT
+  # I = 0.38583
+
+
+  
   
 endfunction
